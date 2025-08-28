2f4a5f37c140cb02ecd67409c42c64e3b38c3382
Omar <Omar@gmail.com>
2025-08-28 16:13:09 +0300

    done Archive

--- src/archive/archive.service.ts
+++ src/archive/archive.service.ts
@@ -1,110 +1,92 @@
-// import { Injectable } from "@nestjs/common";
-// import { CreateArchiveDto } from "./dto/create_Archive.dto";
-// import { OwnerDto } from "./dto/owner.dto";
-// import { ClientDto } from "./dto/client.dto";
-// import { InjectRepository } from "@nestjs/typeorm";
-// import { Archive } from "./entities/archive.entity";
-// import { Repository } from "typeorm";
-// import { Owner } from "./entities/owner.entity";
-// import { Client } from "./entities/client.entity";
-// import { GeneralStatisticsService } from "src/statistics/services/property-statistics.service";
-
-import { Injectable } from "@nestjs/common";
-
-
-// @Injectable()
-// export class ArchiveService{
-
-//     constructor(
-//         @InjectRepository(Archive)
-//         private readonly archiveRepository: Repository<Archive>,
-
-//         @InjectRepository(Owner)
-//         private readonly ownerRepository: Repository<Owner>,
-
-//         @InjectRepository(Client)
-//         private readonly clientRepository: Repository<Client>,
-
-//         private readonly generalStatisticsService: GeneralStatisticsService
-        
-//     ){}
-
-//     /**
-//      * this method is to add a new done property and to the archive,
-//      * or add a new data to an existed property in the archive
-      
-//      * @param createArchiveDto data for the archive
-//      * @param ownerDto data for the owner
-//      * @param clientDto date for the client
-//      * @returns a successfull add or failed add message
-//      */
-
-//     public async addPropertyToArchive(
-//         createArchiveDto: CreateArchiveDto,
-//         ownerDto: OwnerDto,
-//         clientDto: ClientDto
-//     ){
-
-//         const {property_Number , location_Id} = createArchiveDto;
-
-//         const {
-//             owner_personal_Identity_Number,
-//             owner_name,
-//             price,
-//             date,
-//             type,
-//             sell_Date,
-//             rental_Start_Date,
-//             rental_End_Date
-//         } = ownerDto;
-
-//         const {
-//             client_personal_Identity_Number,
-//             client_name
-//         } = clientDto; 
-
-//         var archive = await this.archiveRepository.findOne({
-//             where: {property_Number: property_Number}
-//         });
-
-//         if(!archive){
-//             archive = this.archiveRepository.create({
-//                 property_Number: property_Number,
-//                 location: location_Id
-//             });
-
-//             await this.archiveRepository.save(archive);
-//         }
-
-//         const client = this.clientRepository.create({
-//             personal_Identity_Number: client_personal_Identity_Number,
-//             name: client_name,
-//             archive: archive
-//         });
-
-//         await this.clientRepository.save(client);
-
-
-//         const owner = this.ownerRepository.create({
-//             personal_Identity_Number: owner_personal_Identity_Number,
-//             name : owner_name,
-//             price: price,
-//             date: date,
-//             type: type,
-//             sell_Date: sell_Date,
-//             rental_Start_Date: rental_Start_Date,
-//             rental_End_Date: rental_End_Date,
-//             archive: archive,
-//             client: client
-//         });
-
-//         await this.ownerRepository.save(owner);
-
-//         this.generalStatisticsService.updateStatistic(type);
-
-//         return "A New Data added successfully to the archive";
-//     }
-// }
+import { Injectable } from '@nestjs/common';
+import { CreateArchiveDto } from './dto/create_Archive.dto';
+import { CreateRecordDto } from './dto/create-record.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Archive } from './entities/archive.entity';
+import { Repository } from 'typeorm';
+import { Record } from './entities/record.entity';
+import { Location } from 'src/property/entities/location.entity';
 
 @Injectable()
-export class ArchiveService{}
+export class ArchiveService {
+  constructor(
+    @InjectRepository(Archive)
+    private readonly archiveRepository: Repository<Archive>,
+
+    @InjectRepository(Record)
+    private readonly recordRepository: Repository<Record>,
+
+    @InjectRepository(Location)
+    private readonly locationRepository: Repository<Location>,
+  ) {}
+
+  /**
+     * this method is to add a new done property and to the archive,
+     * or add a new data to an existed property in the archive
+      
+     * @param createArchiveDto data for the archive
+     * @param ownerDto data for the owner
+     * @param clientDto date for the client
+     * @returns a successfull add or failed add message
+     */
+
+  public async addPropertyToArchive(
+    createArchiveDto: CreateArchiveDto,
+    createRecordDto: CreateRecordDto,
+  ) {
+    var archive = await this.archiveRepository.findOne({
+      where: { property_Number: createArchiveDto.property_Number },
+    });
+
+    if (!archive) {
+      const location = await this.locationRepository.findOne({
+        where: { id: createArchiveDto.location_Id },
+      });
+
+      if (location)
+        archive = await this.archiveRepository.create({
+          property_Number: createArchiveDto.property_Number,
+          propertyType: createArchiveDto.propertyType,
+          typeOfPropertyType: createArchiveDto.typeOfPropertyType,
+          space: createArchiveDto.space,
+          location: location,
+        });
+    }
+
+    var newRecord;
+
+    if (archive) {
+       newRecord = this.recordRepository.create({
+        owner_personal_Identity_Number:
+          createRecordDto.owner_personal_Identity_Number,
+        owner_name: createRecordDto.owner_name,
+        client_personal_Identity_Number:
+          createRecordDto.client_personal_Identity_Number,
+        client_name: createRecordDto.client_name,
+        price: createRecordDto.price,
+        type: createRecordDto.type,
+        sell_Date: createRecordDto.sell_Date,
+        rental_End_Date: createRecordDto.rental_End_Date,
+        rental_Start_Date: createRecordDto.rental_Start_Date,
+        archive: archive,
+      });
+    }
+
+    return {message: "Archive Updated successfully"}
+  }
+
+  async getAllArchive(){
+    return await this.archiveRepository.find({
+        relations:['records']
+    });
+  }
+
+  async getOneArchiveWithRecords(archiveId: string){
+    const archive = await this.archiveRepository.findOne({
+        where: {id: archiveId},
+        relations: ['records']
+    });
+
+    return archive;
+  }
+}
--- src/statistics/statistics.module.ts
+++ src/statistics/statistics.module.ts
@@ -1,27 +1,34 @@
-import { Module } from "@nestjs/common";
-import { StatisticsController } from "./statistics.controller";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Archive } from "src/archive/entities/archive.entity";
-import { Owner } from "src/archive/entities/owner.entity";
-import { Property } from "src/property/entities/property.entity";
-import { SubscriptionStatistics } from "./entities/subscription-statistics.entity";
-import { SubscriptionModule } from "src/subscription/subscription.module";
-import { PropertyStatisticsService } from "./services/property-statistics.service";
-import { SubscriptionStatisticsService } from "./services/subscription-statistics.service";
-import { PropertyStatistics } from "./entities/property-statistics.entity";
-import { GeneralStatisticsService } from "./services/general-statistics.service";
-import { generalStatistics } from "./entities/general-statistics.entity";
-
+import { Module } from '@nestjs/common';
+import { StatisticsController } from './statistics.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Archive } from 'src/archive/entities/archive.entity';
+import { Property } from 'src/property/entities/property.entity';
+import { SubscriptionStatistics } from './entities/subscription-statistics.entity';
+import { SubscriptionModule } from 'src/subscription/subscription.module';
+import { PropertyStatisticsService } from './services/property-statistics.service';
+import { SubscriptionStatisticsService } from './services/subscription-statistics.service';
+import { PropertyStatistics } from './entities/property-statistics.entity';
+import { GeneralStatisticsService } from './services/general-statistics.service';
+import { generalStatistics } from './entities/general-statistics.entity';
 
 @Module({
-    imports: [
-        TypeOrmModule.forFeature([
-        SubscriptionStatistics,
-        PropertyStatistics,
-        generalStatistics,
-    ]),],
-    controllers: [StatisticsController],
-    providers: [PropertyStatisticsService, SubscriptionStatisticsService, GeneralStatisticsService],
-    exports: [PropertyStatisticsService, SubscriptionStatisticsService, GeneralStatisticsService],
+  imports: [
+    TypeOrmModule.forFeature([
+      SubscriptionStatistics,
+      PropertyStatistics,
+      generalStatistics,
+    ]),
+  ],
+  controllers: [StatisticsController],
+  providers: [
+    PropertyStatisticsService,
+    SubscriptionStatisticsService,
+    GeneralStatisticsService,
+  ],
+  exports: [
+    PropertyStatisticsService,
+    SubscriptionStatisticsService,
+    GeneralStatisticsService,
+  ],
 })
-export class StatisticsModule { }
+export class StatisticsModule {}
